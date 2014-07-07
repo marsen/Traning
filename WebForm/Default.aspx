f761b32203e4126f91727f7c9253912fe8efb0cb
marsen <thisismysoul@gmail.com>
2014-07-08 07:22:13 +0800

    HTML CSS JAVASCRIPT 綜合練習

--- WebForm/Default.aspx
+++ WebForm/Default.aspx
@@ -5,7 +5,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <script src="JS/jquery-1.11.1.min.js"></script>
+    <script src="JS/Default.js"></script>
 </head>
+
 <body>
     <form id="form1" runat="server">
     <div>
@@ -14,3 +17,5 @@
     </form>
 </body>
 </html>
+<script type="text/template">
+</script>
---
+++ WebForm/JS/clock.js
@@ -0,0 +1,23 @@
+﻿window.onload = function () {
+    
+    var timer = setInterval(function () { clockStart(); }, 1000);
+
+    function clockStart() {
+        var hourPoint = document.getElementsByClassName("hand hour")[0],
+            minutePoint = document.getElementsByClassName("hand minute")[0],
+            secondsPoint = document.getElementsByClassName("hand second")[0];
+        var date = new Date();
+        var seconds = date.getSeconds() * 6,
+            minute = date.getMinutes() * 6,
+            hours = date.getHours() * 30 + (minute / 12);        
+        secondsPoint.style.webkitTransform = "rotate(" + seconds + "deg)";
+        minutePoint.style.webkitTransform = "rotate(" + minute + "deg)";
+        hourPoint.style.webkitTransform = "rotate(" + hours + "deg)";
+    }
+
+    function myStopFunction() {
+        clearInterval(timer);
+    }
+    
+};
+
