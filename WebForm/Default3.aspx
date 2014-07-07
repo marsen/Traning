8fd4b4e9d73b01506ac45d2eb44faac5ed464d10
marsen <thisismysoul@gmail.com>
2014-07-08 07:39:45 +0800

    SESSION 考題

---
+++ WebForm/Default3.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default3.aspx.cs" Inherits="Default3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Text="Clear" OnClick="Button2_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebForm/Default3.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["Name"] != null)
+        {
+            TextBox1.Text = Session["Name"].ToString();
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Session["Name"] = TextBox1.Text;
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Session.Clear();
+    }
+}
